d470954241ee83b8e5f9b77c786705bbf2c1bcc3
jessicamlee <jessicamlee25@gmail.com>
2022-06-01 21:56:29 -0700

    Cleaned up excess and revised names
    Cleaned up excess and revised names

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,10 @@
-// console.log('if you see this, it works');
-
 // To Do List
-// Ref: https://www.w3schools.com/howto/howto_js_todolist.asp?fbclid=IwAR3MiSrU5fKR_iG2yaaaknf-GohUZWEn1yG5p98aFDls5-7zQ6pDla4DDAs
-
 // "close" button + append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
-    // u00D7 is the multiplication symbol
     span.className = "close";
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
@@ -25,7 +20,7 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
-// "checked" symbol when clicking on a list item
+// "checked" symbol
 var list = document.querySelector('ul');
 
 list.addEventListener('click', function(ev) {
@@ -34,24 +29,22 @@ list.addEventListener('click', function(ev) {
   }
 }, false);
 
-// new list item when clicking on the "Add" button
-function newElement() {
+// new list item
+function newItem() {
     var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
+    var inputValue = document.getElementById("myAnswer").value;
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
 
     if (inputValue === '') {
-// Change it from an alert to something else
         alert("You must write something!");
         } else {
         document.getElementById("myUl").appendChild(li);
     }
-    document.getElementById("myInput").value = "";
+    document.getElementById("myAnswer").value = "";
   
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
-    // \u00D7 is a multiplication sign
     
     span.className = "close";
     span.appendChild(txt);
@@ -68,10 +61,7 @@ function newElement() {
 /* -----------------------------
 ------ Dark Mode Toggle --------
 ------------------------------ */
-// Ref: https://codepen.io/PaulinaSurazynska/pen/bGVpBOb
-
 const checkbox = document.getElementById('checkbox');
-
 checkbox.addEventListener('change', ()=>{
     document.body.classList.toggle('light');
 })
