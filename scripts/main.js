ee75c0d4855612d86a488d1d3592f488d64f31fe
jessicamlee <jessicamlee25@gmail.com>
2022-11-17 15:36:32 -0800

    Added simple pomodoro timer
    Added simple pomodoro timer

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,70 @@
 /* -----------------------------
------- Clock/Timer -------------
+------ Pomodoro Timer ----------
 ------------------------------ */
+let seconds = document.querySelector(".seconds");
+let minutes = document.querySelector(".minutes");
 
+let play = document.querySelector(".fa-play");
+let pause = document.querySelector(".fa-pause");
+let stop = document.querySelector(".fa-stop");
+let countdown = -1;
+let pomodoroTimer = 25 * 60;
+let timerValue = pomodoroTimer;
+
+play.addEventListener("click", () => {
+  if (countdown == -1 && timerValue > 0) {
+    countdown = setInterval(() => {
+      let rest = --timerValue;
+      let min = Math.floor(rest / 60);
+      let sec = Math.floor(rest % 60);
+      seconds.textContent = sec.toString().padStart(2, "0");
+      minutes.textContent = min.toString().padStart(2, "0");
+
+      play.style.color = '#7c8dfb';
+      pause.style.color = '#fff';
+      stop.style.color = '#fff';
+
+      if (rest == 0) {
+        clearInterval(countdown);
+      }
+
+    }, 1000);
+  }
+});
+
+pause.addEventListener("click", () => {
+  clearInterval(countdown);
+  countdown = -1;
+  pause.style.color = '#ffcf4A';
+  play.style.color = '#fff';
+  stop.style.color = '#fff';
+});
+
+stop.addEventListener("click", () => {
+  clearInterval(countdown);
+  countdown = -1;
+  timerValue = pomodoroTimer;
+  let min = Math.floor(timerValue / 60);
+  let sec = Math.floor(timerValue % 60);
+  seconds.textContent = sec.toString().padStart(2, "0");
+  minutes.textContent = min.toString().padStart(2, "0");
+  stop.style.color = '#eb0000';
+  play.style.color = '#fff';
+  pause.style.color = '#fff';
+});
 
 /* -----------------------------
 ------ To Do List Field --------
 ------------------------------ */
 // "Close" button + append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
+var myToDolist = document.getElementsByTagName("LI");
 var i;
-for (i = 0; i < myNodelist.length; i++) {
+for (i = 0; i < myToDolist.length; i++) {
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+    myToDolist[i].appendChild(span);
 }
 
 // Close button to hide the current list item
