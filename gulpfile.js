5d72d15ed528b68b389f33660bd7ee10db7025b3
jessicamlee <jessicamlee25@gmail.com>
2022-11-19 12:28:23 -0800

    Add build and watch tasks for main.js
    Add build and watch tasks for main.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-// The second require in Line 2 defines the dependency
 const sourcemaps = require('gulp-sourcemaps');
-// Line 4 links the sourcemaps needed for main.css to load on most browsers
+const concat = require('gulp-concat');
+const minify = require('gulp-minify');
 
 function build_css() {
 
@@ -32,7 +32,26 @@ function build_css() {
 // Make the function available
 exports.build_css = build_css;
 
-// Get Gulp/SASS to watch the styles for changes and will run the "build" script (aka the build_css function)
+function build_js() {
+    // Files are concatenated in the order they're put in
+    return gulp.src([
+            './src/js/aos/aos.js', 
+            './src/js/main.js',])
+        // 
+        .pipe(concat({ path: 'main.js'}))
+        .pipe(minify({
+            ext:{
+                min:'.js'
+            },
+        }))
+        // Build into the build/js folder
+        .pipe(gulp.dest('./build/js/'));
+};
+
+exports.build_js = build_js;
+
+// Watch the styles and scripts for changes to rebuild the files
 exports.watch = function () {
-    gulp.watch('./src/sass/**/*.scss', gulp.series(build_css) );
+    gulp.watch('./src/sass/**/*.scss', gulp.series(build_css));
+    gulp.watch('./src/js/**/*.js', gulp.series(build_js));
 }
