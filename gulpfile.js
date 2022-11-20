ba2dfed4499555950237a6cef063196e1b5fb295
jessicamlee <jessicamlee25@gmail.com>
2022-11-20 11:07:16 -0800

    Fixed JS minifying issue in gulpfile setup
    Fixed JS minifying issue in gulpfile setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,13 @@
-const gulp = require('gulp');
+const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const terser = require('gulp-terser');
 const concat = require('gulp-concat');
-const minify = require('gulp-minify');
 
 function build_css() {
 
-    return gulp.src('./src/sass/**/main.scss')
-        /* Return gulp method of our object. 
-        1: Source file into the source file into the folder and any subfolder, looking for the scss file */
-
-        // Pipe commands:
+    return src('./src/sass/**/main.scss')
+        // Add source files into respective (sub)folders
 
         // Include the pipe command for sourcemaps
         .pipe(sourcemaps.init())
@@ -21,37 +18,30 @@ function build_css() {
             outputStyle: 'compressed'
         }).on('error', sass.logError))
 
-        // Print the sourcemap into the main.css file
+        // Print the sourcemap into the same main.css file
         .pipe(sourcemaps.write())
 
         // Build into the build/css folder
-        .pipe(gulp.dest('./build/css'));
+        .pipe(dest('./build/css'));
     
 }
 
-// Make the function available
-exports.build_css = build_css;
-
 function build_js() {
-    // Files are concatenated in the order they're put in
-    return gulp.src([
+    // Files are concatenated in the order that they would be printed in the destination folder/file
+    return src([
             './src/js/aos/aos.js', 
             './src/js/main.js',])
-        // 
+        .pipe(sourcemaps.init())
+        .pipe(terser())
         .pipe(concat({ path: 'main.js'}))
-        .pipe(minify({
-            ext:{
-                min:'.js'
-            },
-        }))
-        // Build into the build/js folder
-        .pipe(gulp.dest('./build/js/'));
+        .pipe(sourcemaps.write())
+        .pipe(dest('./build/js/'));
 };
 
-exports.build_js = build_js;
-
-// Watch the styles and scripts for changes to rebuild the files
-exports.watch = function () {
-    gulp.watch('./src/sass/**/*.scss', gulp.series(build_css));
-    gulp.watch('./src/js/**/*.js', gulp.series(build_js));
+function watchTasks() {
+    watch('./src/sass/**/*.scss', build_css);
+    watch('./src/js/**/*.js', build_js);
 }
+
+// Add 'exports' for gulp to perform the series of functions
+exports.default = series(build_css, build_js, watchTasks);
